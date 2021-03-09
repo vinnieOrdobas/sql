14dce9d9374c51e784fddec510a7669ab9140409
vinnieOrdobas <viniordobas@icloud.com>
2021-03-09 11:34:52 +0000

    database updated

--- mysql-python.py
+++ mysql-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from Cloud9 workspace
@@ -16,10 +17,12 @@ connection = pymysql.connect(host='localhost',
 try:
     #Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['Jim','Fred']
+        # Prepare a string with the same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings),list_of_names)
+        connection.commit()
+            
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
